55e765c73b2bf3587136b8a49a4450686516af4e
caipeca <loris.kupessa@gmail.com>
2023-07-03 09:27:34 +0100

    Adição da funcao login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HeaderType } from 'src/app/enum/header-type.enum';
+import { User } from 'src/app/model/user';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { NotificationService } from 'src/app/service/notification.service';
 
@@ -10,6 +14,7 @@ import { NotificationService } from 'src/app/service/notification.service';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+  private Subscription: Subscription[] = [];
   constructor(private router: Router, private authenticationService: AuthenticationService, private notifier: NotificationService){}
 
   ngOnInit(): void {
@@ -20,8 +25,25 @@ export class LoginComponent implements OnInit, OnDestroy {
       }
   }
 
-  ngOnDestroy(): void {
+  onLogin(user: User): void{
+    this.Subscription.push(
+      this.authenticationService.login(user).subscribe(
+        (response: HttpResponse<User>)=>{
+          const token = response.headers.get(HeaderType.JWT_TOKEN);
+          this.authenticationService.saveToken(token);
+          this.authenticationService.addUserToLocalCache(response.body);
+          this.router.navigateByUrl('/manutenca/home');
+        },
+        (errorResponse: HttpErrorResponse)=>{
+          console.log(errorResponse);
+          this.notifier.openSnackBar(errorResponse.message);
+        }
+      )
+    )
+  }
 
+  ngOnDestroy(): void {
+    this.Subscription.forEach(sub => sub.unsubscribe());
   }
 
 }
